9bb83059cd6abd63802cb950324216d3737599b3
Zihao Chen <z5545252@ad.unsw.edu.au>
2026-04-17 15:07:24 +1000

    fix lint bug in landingpage

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,13 +1,13 @@
 import { useNavigate }  from 'react-router-dom';
 
 const LandingPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <div>
-        <h1>Landing Page</h1>
-        <button onClick={() => navigate('/login')}>Login</button>
-        <button onClick={() => navigate('/register')}>Register</button> 
+      <h1>Landing Page</h1>
+      <button onClick={() => navigate('/login')}>Login</button>
+      <button onClick={() => navigate('/register')}>Register</button> 
     </div>
   );
 };
