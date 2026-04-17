86eb30791e7094d001eb55e7565e3942a75c9d92
Zihao Chen <z5545252@ad.unsw.edu.au>
2026-04-17 15:13:32 +1000

    fix lint bug in editcodemodal part 1

--- frontend/src/components/EditCodeModal.tsx
+++ frontend/src/components/EditCodeModal.tsx
@@ -8,38 +8,38 @@ interface EditCodeModalProps {
 }
 
 const EditCodeModal = ({ element, onClose, onSubmit }: EditCodeModalProps) => {
-    const [code, setCode] = useState('');
-    const [fontSize, setFontSize] = useState(1);
-    const [width, setWidth] = useState(50);
-    const [height, setHeight] = useState(30);
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
+  const [code, setCode] = useState('');
+  const [fontSize, setFontSize] = useState(1);
+  const [width, setWidth] = useState(50);
+  const [height, setHeight] = useState(30);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
 
-    useEffect(() => {
-        if (element) {
-            setCode(element.code);
-            setFontSize(element.fontSize);
-            setWidth(element.width);
-            setHeight(element.height);
-            setX(element.x);
-            setY(element.y);
-        }
-    }, [element]);
+  useEffect(() => {
+    if (element) {
+      setCode(element.code);
+      setFontSize(element.fontSize);
+      setWidth(element.width);
+      setHeight(element.height);
+      setX(element.x);
+      setY(element.y);
+    }
+  }, [element]);
 
-    if (!element) return null;
+  if (!element) return null;
 
-    const handleSubmit = () => {
-        onSubmit({
-            ...element,
-            code,
-            fontSize,
-            width,
-            height,
-            x,
-            y,
-        });
-        onClose();
-    };
+  const handleSubmit = () => {
+    onSubmit({
+      ...element,
+      code,
+      fontSize,
+      width,
+      height,
+      x,
+      y,
+    });
+    onClose();
+  };
 
     return (
         <div 
